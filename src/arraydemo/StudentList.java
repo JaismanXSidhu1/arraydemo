b962ebcf8efc5105f9cadf346fed5f2b7a3650b9
Jaisman Singh Sidhu <sidhu722@sheridancollege.ca>
2024-01-17 09:59:52 -0500

    Third commit added print in Student.java file

--- src/arraydemo/StudentList.java
+++ src/arraydemo/StudentList.java
@@ -13,6 +13,7 @@ public class StudentList {
         Student s1 = new Student(1, "Jaisman");
         Student[] StudentList = new Student[3];
         StudentList[0]=s1;
+        System.out.println("Third Comment");
         StudentList[1] = new Student(2, "K");
         StudentList[2]  = new Student(3, "O");
         
